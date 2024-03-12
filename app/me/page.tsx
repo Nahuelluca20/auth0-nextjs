673d1720a414b50f104081e99063f94a9808d955
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-12 15:33:20 -0300

    update some styles

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,18 +1,46 @@
 import {getSession} from "@auth0/nextjs-auth0";
 import Image from "next/image";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
 export default async function Page() {
   const {user}: any = await getSession();
 
-  return user ? (
-    <div>
-      <Image alt={user.name} height={50} src={user.picture} width={50} />
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-    </div>
-  ) : (
-    <div>
-      <h3>login for see your user</h3>
-    </div>
+  return (
+    <section className="flex min-h-screen flex-col items-center p-24">
+      {user ? (
+        <Card className="max-w-[300px]  grid justify-center items-center">
+          <CardHeader>
+            <CardTitle>My User</CardTitle>
+            <Image
+              alt={user.name}
+              className="rounded-full"
+              height={80}
+              src={user.picture}
+              width={80}
+            />
+          </CardHeader>
+          <CardContent>
+            <h2 className="text-lg font-semibold">Name: {user.name}</h2>
+            <p>Email: {user.email}</p>
+          </CardContent>
+          <CardFooter>
+            <Button variant={"destructive"}>LogOut</Button>
+          </CardFooter>
+        </Card>
+      ) : (
+        <div>
+          <h3>login for see your user</h3>
+        </div>
+      )}
+    </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>HOLA</h1>
-      <Button asChild>
-        <Link href="/me">User</Link>
-      </Button>
-      <Link href="/api/auth/login">Login</Link>
+    <main className="flex min-h-screen gap-5 flex-col items-center p-24">
+      <h1 className="text-3xl font-semibold">NextJS + Auth0</h1>
+      <div className="space-x-5">
+        <Button asChild>
+          <Link href="/api/auth/login">Login</Link>
+        </Button>
+        <Button asChild variant={"outline"}>
+          <Link href="/me">User</Link>
+        </Button>
+      </div>
     </main>
   );
 }
