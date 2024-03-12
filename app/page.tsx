1869882bf00991ef782c4d76018ead799b1baa92
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-12 17:43:36 -0300

    add protected-route and middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
 import {Button} from "@/components/ui/button";
+import SessionButton from "@/components/session-button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen gap-5 flex-col items-center p-24">
       <h1 className="text-3xl font-semibold">NextJS + Auth0</h1>
-      <div className="space-x-5">
-        <Button asChild>
-          <Link href="/api/auth/login">Login</Link>
-        </Button>
-        <Button asChild variant={"outline"}>
-          <Link href="/me">User</Link>
-        </Button>
-      </div>
+      <SessionButton />
+      <Button asChild variant={"secondary"}>
+        <Link href="/me">User</Link>
+      </Button>
+      <Button asChild variant={"outline"}>
+        <Link href="/protected-route">Protected route</Link>
+      </Button>
     </main>
   );
 }
---
+++ app/protected-route/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex gap-5 min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl">This is a protected route</h1>
+    </div>
+  );
+}
