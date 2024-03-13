14ca5c7d6dfd19eb3c43bc59292329394f3f8c1e
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-13 00:03:34 -0300

    Refactor route.ts file for better readability and error handling

--- app/api/[...proxy]/route.ts
+++ app/api/[...proxy]/route.ts
@@ -1,15 +1,18 @@
-import { getAccessToken } from '@auth0/nextjs-auth0/edge';
-import { NextRequest, NextResponse } from 'next/server';
+import {getAccessToken} from "@auth0/nextjs-auth0/edge";
+import {NextRequest, NextResponse} from "next/server";
 
 const handler = async (request: NextRequest) => {
   let token = null;
+
   try {
     const response = new NextResponse();
-    const { accessToken } = await getAccessToken(request, response);
+    const {accessToken} = await getAccessToken(request, response);
+
     token = accessToken;
   } catch (e: any) {
-    console.error('Cannot get access token.');
+    console.error("Cannot get access token.");
     console.log(e);
+
     return new Response(e, {
       status: 500,
     });
@@ -17,13 +20,13 @@ const handler = async (request: NextRequest) => {
 
   try {
     if (token) {
-      request.headers.set('Authorization', `Bearer ${token}`);
+      request.headers.set("Authorization", `Bearer ${token}`);
     }
 
     // Extract the necessary components from the original request
     const originalURL = new URL(request.url);
     const basePath = process.env.NEXT_PUBLIC_API_BASE_URL; // Your base path
-    const newPath = originalURL.pathname.replace(/^/api/proxy/, '');
+    const newPath = originalURL.pathname.replace("/api/proxy", "sarasa");
     const queryString = originalURL.search; // Query string
     const hash = originalURL.hash; // Hash
 
@@ -32,7 +35,7 @@ const handler = async (request: NextRequest) => {
     const newURL = new URL(newURLString);
 
     // Next.js adapted the fetch implementation that forces caching on a success response. We don't want that.
-    const response = await fetch(newURL.toString(), { ...request, cache: 'no-store' });
+    const response = await fetch(newURL.toString(), {...request, cache: "no-store"});
 
     if (response.status === 401) {
       console.error(`Received 401 Unauthorized from backend API.`);
@@ -46,6 +49,7 @@ const handler = async (request: NextRequest) => {
   } catch (e: any) {
     console.error(`Received 500 Internal Server Error from backend API.`);
     console.log(e);
+
     return new Response(e, {
       status: 500,
     });
